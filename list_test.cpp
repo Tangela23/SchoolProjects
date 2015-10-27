8294d19f715e1aae5ab9addb3d1ca71eb27f955b
Tangela923 <Tangela923@users.noreply.github.com>
2015-10-27 17:20:05 -0600

    Create list_test.cpp

---
+++ list_test.cpp
@@ -0,0 +1,31 @@
+/*
+  list_test.cpp
+
+  Description:
+    School project. C++
+    Program to test the singly-linked list class.
+*/
+
+#include <iostream.h>
+#include <string.h>
+#include "list.H"
+
+void main(){
+	List mylist;
+
+	mylist.Pop();
+	mylist.Push(5);
+	mylist.Push(3);
+	mylist.Push(8);
+	mylist.Push(2);
+	mylist.Push(7);
+	mylist.Push(1);
+	mylist.Pop();
+
+	mylist.Print();
+
+	mylist.BubbleSort();
+
+  cout<<"Sorted list: "<<endl;
+	mylist.Print();
+}
