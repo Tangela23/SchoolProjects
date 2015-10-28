fa78e7a94415f9b6616233a12445756d4e7f0f78
Tangela923 <Tangela923@users.noreply.github.com>
2015-10-28 08:45:04 -0600

    Update stats_test.cpp

--- stats_test.cpp
+++ stats_test.cpp
@@ -24,68 +24,68 @@ void main()
 
   /* Get the user input. */
   cout<<"Total number of elements (max is "<<MAX_N<<"): ";
-	cin>>n;
+  cin>>n;
   n = (n <= MAX_N) ? n : MAX_N;
   
-	cout<<"\nEnter the elements one by one:"<<endl;
-	for (i = 0; i < n; i++)
-	{
-		cin>>data_set[i];
-	}
+  cout<<"\nEnter the elements one by one:"<<endl;
+  for (i = 0; i < n; i++)
+  {
+    cin>>data_set[i];
+  }
   
   /* Calculate the mean (usual average). */
-	for (i = 0; i < n; i++)
-	{
-		count += data_set[i];
-	}
-	mean = count / n;
+  for (i = 0; i < n; i++)
+  {
+    count += data_set[i];
+  }
+  mean = count / n;
 
   /* Bubble sort the array of integers. */
-	for (i = 0; i < (n - 1); i++)
-	{
-		for (j = (i + 1); j < n; j++)
-		{
-			if (data_set[j] < data_set[i])
-			{
-				k = data_set[j];
-				data_set[j] = data_set[i];
-				data_set [i] = k;
-			}
-		}
-	}
+  for (i = 0; i < (n - 1); i++)
+  {
+    for (j = (i + 1); j < n; j++)
+    {
+      if (data_set[j] < data_set[i])
+      {
+        k = data_set[j];
+        data_set[j] = data_set[i];
+        data_set [i] = k;
+      }
+    }
+  }
   /* Print the sorted array. */
-	for (i = 0; i < n; i++)
-		cout<<data_set[i]<<"-";
+  for (i = 0; i < n; i++)
+    cout<<data_set[i]<<"-";
 
   /* Calculate the median (middle number) using the sorted array. If there
    * is an even number of numbers, the median is the average between the two
    * middle numbers. */
-	k = n / 2;
-	if ((n % 2) == 0)
-		median = (data_set[k] + data_set[k - 1]) / 2;
-	else
-		median = data_set[k];
+  k = n / 2;
+  if ((n % 2) == 0)
+    median = (data_set[k] + data_set[k - 1]) / 2;
+  else
+    median = data_set[k];
 
   /* Find the mode (element that repeats the most) in the sorted array. */
-	max_count = 1;
-	for (i = 1; i < n; i++)
-	{
+  max_count = 1;
+  for (i = 1; i < n; i++)
+  {
     k = data_set[i];
     count = 0;
 
     /* Count how many times k appears in the data set. */
-		while (k == data_set[i])
-		{
-			count++;
-			i++;
-		}
+    while (k == data_set[i])
+    {
+      count++;
+      i++;
+    }
     /* Keep the maximum number of times an integer repeats in the data set. */
-		if (count > max_count)
-		{
-			max_count = count;
-			mode = data_set[i - 1];
-		}
-	}
+    if (count > max_count)
+    {
+      max_count = count;
+      mode = data_set[i - 1];
+    }
+  }
   /* If all integers appear just once in the data set, there is no mode. */
   if (max_count == 1)
     mode = 0;
